3de7bb94f64a8b6f7bd6f2e7d4e1ddc568813ccf
Web0zz <furkan.ozen4@ogr.sakarya.edu.tr>
2021-09-25 00:40:35 +0300

    player state listener added

--- app/src/main/java/com/web0zz/science_news/screen/overview/video/VideoFragment.kt
+++ app/src/main/java/com/web0zz/science_news/screen/overview/video/VideoFragment.kt
@@ -2,7 +2,9 @@ package com.web0zz.science_news.screen.overview.video
 
 import android.os.Bundle
 import android.view.View
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.util.Util
 import com.web0zz.science_news.R
@@ -23,6 +25,7 @@ class VideoFragment : BaseFragment<ViewVideoOverviewBinding>() {
     private var playWhenReady = true
     private var currentWindow = 0
     private var playbackPosition = 0L
+    private val playbackStateListener: Player.Listener = playbackStateListener()
 
     override fun Bundle.getArgumentsToVariable() {
         shortVideoId = this.getInt(CURRENT_OVERVIEW_ID)
@@ -60,15 +63,12 @@ class VideoFragment : BaseFragment<ViewVideoOverviewBinding>() {
     }
 
     private fun initializePlayer() {
-        fragmentDataBinding.videoGroup.visibility = View.VISIBLE
-        fragmentDataBinding.loadingProgressBar.visibility = View.GONE
-
-        // TODO will add player event listener
-
         player = SimpleExoPlayer.Builder(requireContext())
             .build()
             .also { exoPlayer ->
                 fragmentDataBinding.shortExoPlayer.player = exoPlayer
+                exoPlayer.seekTo(currentWindow, playbackPosition)
+                exoPlayer.addListener(playbackStateListener)
                 val mediaItem = MediaItem.fromUri(shortVideo.videoUrl)
                 exoPlayer.setMediaItem(mediaItem)
                 exoPlayer.playWhenReady = playWhenReady
@@ -82,9 +82,19 @@ class VideoFragment : BaseFragment<ViewVideoOverviewBinding>() {
             playbackPosition = this.currentPosition
             currentWindow = this.currentWindowIndex
             playWhenReady = this.playWhenReady
+            removeListener(playbackStateListener)
             release()
         }
         player = null
+    }
+
+    private fun playbackStateListener() = object : Player.Listener {
+        override fun onPlaybackStateChanged(playbackState: Int) {
+            if(playbackState == ExoPlayer.STATE_READY) {
+                fragmentDataBinding.videoGroup.visibility = View.VISIBLE
+                fragmentDataBinding.loadingProgressBar.visibility = View.GONE
+            }
+        }
     }
 
     companion object {
